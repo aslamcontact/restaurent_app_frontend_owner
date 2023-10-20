ce489aec106881ac85fd0b0f232dc9402474848a
aslamcontact <aslam.mycontact@gmail.com>
2023-10-20 21:46:49 +0530

    add main->option->menu->{add and remove category}

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { MenuComponent } from './components/main/options/menu/menu.component';
+import { AddCategoryComponent } from './components/main/options/menu/add-category/add-category.component';
+import { RemoveCategoryComponent } from './components/main/options/menu/remove-category/remove-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuComponent,
+    AddCategoryComponent,
+    RemoveCategoryComponent
   ],
   imports: [
     BrowserModule,
